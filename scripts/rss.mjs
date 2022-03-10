e329d4a005d72aee29b7635388a9c659bf46aadb
Yap Khai Chuen <89649333+yapkhaichuen@users.noreply.github.com>
2022-03-10 22:26:00 +0800

    Update rss.mjs

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -13,7 +13,7 @@ async function generate() {
     feed.item({
       title: post.title,
       url: `https://khaichuen.vercel.app/blog/${post.slug}`,
-      pubDate: post.publishedAt,
+      published: post.publishedAt,
       description: post.summary
     });
   });
